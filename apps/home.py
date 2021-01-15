0fc8092bc8c92c2a5c4d4b9ba2970f68d4181fe6
svenson.e <svenson.ertel@gmail.com>
2021-01-15 20:16:28 +0800

    .

--- apps/home.py
+++ apps/home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import base64
 from data.create_data import create_table
 
 def app():
@@ -14,5 +15,27 @@ def app():
     st.write(df)
 
     st.write('Navigate to `Data Stats` page to visualize the data')
+    # Set Background Image *local file" - heroku ?
+    @st.cache(allow_output_mutation=True)
+    def get_base64_of_bin_file(bin_file):
+        with open(bin_file, 'rb') as f:
+            data = f.read()
+        return base64.b64encode(data).decode()
+
+    def set_png_as_page_bg(png_file):
+        bin_str = get_base64_of_bin_file(png_file)
+        page_bg_img = '''
+        <style>
+        body {
+        background-image: url("data:image/png;base64,%s");
+        background-size: cover;
+        }
+        </style>
+        ''' % bin_str
+        
+        st.markdown(page_bg_img, unsafe_allow_html=True)
+        return
+
+    set_png_as_page_bg('bg1.jpg')
 
 
